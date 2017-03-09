b91d2bec8320a5afc63a4d53e8782726810388e5
Chintan Shah <chintan837@gmail.com>
2017-03-09 01:38:42 -0800

    temp

--- mincut.c
+++ mincut.c
@@ -3,7 +3,26 @@
 #include <string.h>
 #include <time.h>
 
+struct node {
+	int value;
+	struct node *next;
+	struct node *prev;
+	// list of edges
+}
+
+struct edge {
+	struct node *a;
+	struct node *b;
+	struct edge *next;
+	struct edge *prev;
+}
+
+
 int main(void) {
+	FILE *fp = fopen("karger_test1.txt", "r");
+	
+
+	fclose (fp);
 
 	printf("mincut\n");
 	return 0;
