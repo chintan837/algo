edcaf02179496a1447155462ddf18105a3096a4c
Chintan Shah <chintan837@gmail.com>
2017-03-28 19:18:28 -0700

    some changes

--- mincut.c
+++ mincut.c
@@ -8,109 +8,199 @@ struct edgelist;
 struct node {
 	int value;
 	struct edgelist *neigh;	// pointer to head of list of edges 
-}*Nodes;
+};
 
 struct edge {
 	struct node *a;
 	struct node *b;
-	struct edgelist *pos;
+	int refcnt;
 };
 
 struct edgelist {
 	struct edge *edge;
 	struct edgelist *next;
-	struct edgelist *prev;
 };
 
-struct edgelist *Edges = NULL;
+struct node *initnnodes(int N) {
+	struct node *nodes = calloc (N+1, sizeof(struct node));
+	for (int i = 1; i <= N; i++) {
+		nodes[i].value = i;
+		nodes[i].neigh = NULL;
+	}
 
-int addneigh(int a, int b) {
-	if (a > b)
-		return 0;
+	return nodes;
+}
 
-	//	printf("Added edge %d -- %d\n", a, b);
+void freennodes(struct node *nodes) {
+	free(nodes);
+}
 
-	struct node *A = &Nodes[a];
-	struct node *B = &Nodes[b];
+void listadd(struct edge *node, struct edgelist **head) {
+	struct edgelist *newedge = malloc(sizeof(struct edgelist));
+
+	node->refcnt++;
+	newedge->edge = node;
+	newedge->next = *head;
+	*head = newedge;
+
+	return;
+}
+
+struct edge *createedge(int a, int b, struct node *nodes) {
+	struct node *A = &nodes[a];
+	struct node *B = &nodes[b];
+
 	struct edge *newedge = malloc (sizeof (struct edge));
 	newedge->a = A;
 	newedge->b = B;
+	newedge->refcnt = 0;
 
-	struct edgelist *newedgelist;
-	newedgelist = malloc (sizeof(struct edgelist));
-	newedge->pos = newedgelist;
-	newedgelist->edge = newedge;
-	newedgelist->next = Edges;
-	Edges = newedgelist;
+	return newedge;
+}
 
-	newedgelist = malloc (sizeof(struct edgelist));
-	newedgelist->edge = newedge;
-	newedgelist->next = A->neigh;
-	A->neigh = newedgelist;
-	
-	newedgelist = malloc (sizeof(struct edgelist));
-	newedgelist->edge = newedge;
-	newedgelist->next = B->neigh;
-	B->neigh = newedgelist;
-	return 1;
+void deleteedge(struct edge *edge) {
+	edge->refcnt--;
+	if (edge->refcnt == 0)
+		free(edge);
+}
+
+void printlist(char *tag, struct edgelist *edges) {
+	printf("%s:\n", tag);
+	while (edges) {
+		printf("\t%d--%d\n", edges->edge->a->value, edges->edge->b->value);
+		edges = edges->next;
+	}
+}
+
+struct edge *pickedge(struct edgelist *edges, int t) {
+	int count  = 0;
+	while (edges && count < t) {
+		edges = edges->next;
+		count++;
+	}
+	return edges->edge;
 }
 
 int purgelist(struct edgelist **list) {
 	int ret = 0;
-	struct edgelist *curr;
-	struct edgelist *prev;
-	
-	curr = *list;
-	prev = NULL;
-	while ((curr = *list) && (curr->edge->a == curr->edge->b)) {
-		printf("examing edge %d--%d\n", curr->edge->a->value, curr->edge->b->value);
-		prev = curr;
-		*list = (*list)->next;
-		free(prev);
+	struct edgelist *current = *list;
+
+	while (current && (current->edge->a->value == current->edge->b->value)) {
+		printf("found head as duplicate duplicate\n");
+		deleteedge(current->edge);
+		*list = current->next;
+		free(current);
 		ret++;
+		current = *list;
 	}
-	while (curr != NULL) {
-		printf("examing edge %d--%d\n", curr->edge->a->value, curr->edge->b->value);
-		prev = curr;
-		prev = curr;
-		curr = curr->next;
-		if (prev->edge->a == prev->edge->b) {
-			free(prev);
+	struct edgelist *prev = current;
+	while (current) {
+		if  (current->edge->a->value == current->edge->b->value) {
+			prev->next = current->next;
+			deleteedge(current->edge);
+			free(current);
 			ret++;
+			current = prev->next;
+		} else {
+			prev = current;
+			current = current->next;
 		}
 	}
 
 	return ret;
 }
 
-int deleteedge(struct edge *edge, int coin) {
-	int ret = 0;
-	if (coin) {
+void randomedge(struct edge *edge) {
+	if (rand()%2) {
 		struct node *tmp = edge->a;
 		edge->a = edge->b;
 		edge->b = tmp;
 	}
+}
 
-	struct edgelist *curr;
-	struct edgelist *prev;
+void sanity() {
+	int n = 4;
+	int m = 0;
+	struct edgelist *edges = NULL;
+	struct edge *edge;
+	struct node *nodes = initnnodes(n);
 
-	curr = edge->a->neigh;
-	prev = NULL;
-	while (curr != NULL) {
-		if (curr->edge->a == edge->a) {
-			curr->edge->a = edge->b;
-		} else {
-			curr->edge->b = edge->b;
-		}
-		prev = curr;
-		curr = curr->next;
-		free(prev);
-	}
-	edge->a->neigh = NULL;
+	edge = createedge(1, 2, nodes);
+	m++;
+	listadd(edge, &edges);
+	edge = createedge(1, 3, nodes);
+	m++;
+	listadd(edge, &edges);
+	edge = createedge(2, 2, nodes);
+	m++;
+	listadd(edge, &edges);
+	edge = createedge(2, 3, nodes);
+	m++;
+	listadd(edge, &edges);
+	edge = createedge(3, 3, nodes);
+	m++;
+	listadd(edge, &edges);
+	edge = createedge(4, 4, nodes);
+	m++;
+	listadd(edge, &edges);
 
-	ret = purgelist(&(edge->b->neigh));
+	printlist("Test print", edges);
 	
-	return ret;
+	int t = rand()%m;
+	t = 4;
+	printf("Picking edge %d of %d\n", t, m);
+	edge = pickedge(edges, t);
+	printf("Edge: %d -- %d\n", edge->a->value, edge->b->value);
+	purgelist(&edges);
+	printlist("after purge", edges);
+
+	freennodes(nodes);
+
+	exit(EXIT_SUCCESS);
+}
+
+
+int addneigh(int a, int b, struct node *nodes, struct edgelist **edges) {
+	if (a > b)
+		return 0;
+
+	struct edge *edge;
+	
+	edge = createedge(a, b, nodes);
+	listadd(edge, edges);
+	listadd(edge, &(nodes[a].neigh));
+	listadd(edge, &(nodes[b].neigh));
+
+	return 1;
+}
+
+void killvictim(struct node *victim, struct node *target) {
+	struct edgelist *current = victim->neigh;
+	struct edgelist *del;
+	printf("deleting node %d\n", victim->value);
+
+	while (current) {
+		if (current->edge->a == victim) {
+			current->edge->a = target;
+		} else {
+			current->edge->b = target;
+		}
+		del = current;
+		current = current->next;
+		deleteedge(del->edge);
+		free(del);
+	}
+	victim->neigh = NULL;
+}
+
+void reduceedge(struct edge *edge) {
+	struct node *victim = edge->a;
+	struct node *target = edge->b;
+
+	printlist("Target neigh before:", target->neigh);
+	killvictim(victim, target);
+	printlist("Target neigh after:", target->neigh);
+	purgelist(&(target->neigh));
 }
 
 int main(void) {
@@ -122,7 +212,10 @@ int main(void) {
 	int N = 0;
 	int M = 0;
 	time_t t;
-	srand(time(&t));
+	//	srand(time(&t));
+	srand(0);
+
+	//	sanity();
 
 	printf("mincut\n");
 	//	fp = fopen("kargerMinCut.txt", "r");
@@ -137,11 +230,9 @@ int main(void) {
 
 	rewind(fp);
 
-	Nodes = calloc (N+1, sizeof(struct node));
-	for (int i = 1; i <= N; i++) {
-		Nodes[i].value = i;
-		Nodes[i].neigh = NULL;
-	}
+	struct node *nodes = initnnodes(N);
+	struct edgelist *edges = NULL;
+	struct edge *edge = NULL;
 
 	while ((read = getline(&line, &len, fp)) != -1) {
 		char *pch;
@@ -154,7 +245,7 @@ int main(void) {
 			b = atoi(pch);
 			pch = strtok(NULL, " \t\n");
 //			printf("\tNieghbor: %d\n", b);
-			M += addneigh(a, b);
+			M += addneigh(a, b, nodes, &edges);
 		}
 	}
 	free(line);
@@ -163,25 +254,22 @@ int main(void) {
 	int n = N;
 	int m = M;
 	while (n > 2) {
-		int target = rand()%m;
-		int coin = rand()%2;
-
-		printf("picked %2d of %2d, coin %d\n", target, m, coin);
-
-		int count = 0;
-		struct edgelist *current = Edges;
-		while (count < target) {
-			count++;
-			current = current->next;
-		}
-		m -= deleteedge(current->edge, coin);
-		printf("Remaining edges = %d\n", m);
+		int t = rand()%m;
+		printlist("Current list:", edges);
+		edge = pickedge(edges, t);
+		randomedge(edge);
+		printf("pciked edge #%d %d--%d\n", t, edge->a->value, edge->b->value);
+		reduceedge(edge);
+		printlist("After reduction:", edges);
+		m -= purgelist(&edges);
+		printlist("After purge:", edges);
+		printf("---------------Remaining edges = %d--------------\n", m);
 		n--;
 	}
 
 	printf("-- Remaining edges = %d --\n", m);
 
-	free(Nodes);
+	freennodes(nodes);
 	fclose (fp);
 
 	return 0;
