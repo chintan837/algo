36a5a0b695bb4ea0822cabe7e5d8b926afc59c2c
Chintan Shah <chintan837@gmail.com>
2017-04-12 20:29:50 -0700

    Fixed parsing. Add carriage return \r as delimiter. reallaoc() error due to variable reuse. Using same variable len in getline was causing issues.

--- dijkstra.c
+++ dijkstra.c
@@ -4,16 +4,22 @@
 
 struct node {
 	int value;
-	struct edgelist;
-}
+	int distance;
+	struct edgelist *edgelist;
+};
 
-struct edgelist {
+struct edge {
 	struct node *node;
 	int len;
-}
+};
+
+struct edgelist {
+	struct edge *edge;
+	struct edgelist *next;
+};
 
 addedge(int s, int t, int len) {
-
+	printf("Adding edge %d -- %d (%d)\n", s, t, len);
 }
 
 int main(int argc, char *argv[]) {
@@ -27,25 +33,23 @@ int main(int argc, char *argv[]) {
 	ssize_t read;
 	char *ptr;
 	char *token;
-	int s = 0, t = 0;
+	int s = 0, t = 0, w = 0;
 
 	while ((read = getline(&line, &len, fp)) != -1) {
 		token = strtok(line, "\t");
 		s = atoi(token);
 		int flag = 1;
-		while (token = strtok(NULL, ",\t\n")){
+		while (token = strtok(NULL, ",\t\n\r")){
 			if (flag) {
 				t = atoi(token);
 				flag = 0;
 			} else {
-				len = atoi(token);
+				w = atoi(token);
 				flag = 1;
-				addedge(s, t, len);
+				addedge(s, t, w);
 			}
 		}
 	}
-
-
 
 	fclose(fp);
 	free(line);
