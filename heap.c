f997dadc76be70002979ea77f710f1a64a031006
Chintan Shah <chintan837@gmail.com>
2017-04-13 23:06:19 -0700

    some more changes

--- heap.c
+++ heap.c
@@ -11,6 +11,24 @@ void swap(int *a, int *b) {
 	*b = tmp;
 }
 
+void swapp(int **a, int **b) {
+	int *tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int heap_add(int **heap, int heap_len, int *p) {
+	
+
+}
+
+int *get_min(int **heap, int *heap_len) {
+	int *current = *heap;
+
+
+	return current;
+}
+
 int main(int argc, char *argv[]) {
 	int array[LEN];
 
@@ -28,9 +46,19 @@ int main(int argc, char *argv[]) {
 	for (int i = 0; i < LEN; i++) {
 		printf("%d ", array[i]);
 	}
-	
-	int **heap = calloc(LEN, 
-
-
 	printf("\n");
+	
+	int **heap = calloc(LEN+1, sizeof(int *));
+	int heap_len = 0;
+	int *p;
+
+	for (int i = 1; i <= LEN; i++) {
+		heap_len = heap_add(heap, heap_len, &array[i]);
+	}
+
+	while (p = get_min(heap, &heap_len)) {
+		printf("%d\n", *p);
+	}
+
+	free(heap);
 }
