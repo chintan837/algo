dbcd85b43d8b158da2805c25e94764a6205e6eac
Chintan Shah <chintan837@gmail.com>
2017-04-16 12:11:47 -0700

    heap_add works fine

--- heap.c
+++ heap.c
@@ -11,15 +11,40 @@ void swap(int *a, int *b) {
 	*b = tmp;
 }
 
+void swapp(int **a, int **b) {
+	int *tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void print_heap(char *label, int **heap, int heap_len) {
+	printf("%s: ", label);
+	for (int i = 1; i <= heap_len; i++) {
+		printf("%d ", *heap[i]);
+	}
+	printf("\n");
+}
+
 int heap_add(int **heap, int heap_len, int *p) {
 	assert(heap_len <= LEN);
-
 
 	heap_len++;
 	heap[heap_len] = p;
 	printf("Added %d to index %d\n", *heap[heap_len], heap_len);
 
 	/* bubble up */
+	int child = heap_len;
+	int parent = heap_len/2;
+	while (parent >= 1) {
+		if (*heap[parent] > *heap[child]) {
+			printf("comparing parent %d[%d] to child %d[%d]\n",
+					*heap[parent], parent, *heap[child], child);
+			swapp(&heap[parent], &heap[child]);
+			child = parent;
+			parent = child/2;
+		} else 
+			break;
+	}
 
 	return heap_len;
 }
@@ -28,10 +53,11 @@ int *get_min(int **heap, int *len) {
 	int heap_len = *len;
 	if (heap_len == 0)
 		return NULL;
-	int *ret = &(*heap[1]);
+
+	int *ret = heap[1];
+	swapp(&heap[1], &heap[heap_len]);
 	--*len;
 
-	printf("Extracted %d from index 1\n", *ret);
 	/* bubble down */
 
 	return ret;
@@ -62,12 +88,15 @@ int main(int argc, char *argv[]) {
 	
 	for (int i = 0; i < LEN; i++) {
 		heap_len = heap_add(heap, heap_len, &array[i]);
+		print_heap("Current heap ", heap, heap_len);
 	}
 
 	printf("added %d elements\n", heap_len);
+	print_heap("Added elements to heap", heap, heap_len);
 
 	int *min = NULL;
 	while ((min = get_min(heap, &heap_len))) {
 		printf("Extracted %d new len: %d\n", *min, heap_len);
+		print_heap("Current heap ", heap, heap_len);
 	}
 }
