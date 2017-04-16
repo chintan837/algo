0b322c5f3985f4bc2ca57aa8e6d36e8c8e7166da
Chintan Shah <chintan837@gmail.com>
2017-04-16 08:20:17 -0700

    lenght not decremented because *len-- instead of --*len

--- heap.c
+++ heap.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <assert.h>
 
 #define LEN 24
 
@@ -11,9 +11,30 @@ void swap(int *a, int *b) {
 	*b = tmp;
 }
 
-int *get_min(int **heap) {
+int heap_add(int **heap, int heap_len, int *p) {
+	assert(heap_len <= LEN);
 
 
+	heap_len++;
+	heap[heap_len] = p;
+	printf("Added %d to index %d\n", *heap[heap_len], heap_len);
+
+	/* bubble up */
+
+	return heap_len;
+}
+
+int *get_min(int **heap, int *len) {
+	int heap_len = *len;
+	if (heap_len == 0)
+		return NULL;
+	int *ret = &(*heap[1]);
+	--*len;
+
+	printf("Extracted %d from index 1\n", *ret);
+	/* bubble down */
+
+	return ret;
 }
 
 int main(int argc, char *argv[]) {
@@ -24,7 +45,8 @@ int main(int argc, char *argv[]) {
 	}
 
 	time_t t;
-	srand(time(&t));
+	//	srand(time(&t));
+	srand((0));
 
 	for (int i = 0; i < LEN; i++) {
 		swap(&array[i], &array[rand()%LEN]);
@@ -33,15 +55,19 @@ int main(int argc, char *argv[]) {
 	for (int i = 0; i < LEN; i++) {
 		printf("%d ", array[i]);
 	}
+	printf("\n");
 	
 	int **heap = calloc(LEN+1, sizeof (int *));
-
+	int heap_len = 0;
 	
 	for (int i = 0; i < LEN; i++) {
-		heap_add(heap);
+		heap_len = heap_add(heap, heap_len, &array[i]);
 	}
-	int *min = get_min(heap);
 
+	printf("added %d elements\n", heap_len);
 
-	printf("\n");
+	int *min = NULL;
+	while ((min = get_min(heap, &heap_len))) {
+		printf("Extracted %d new len: %d\n", *min, heap_len);
+	}
 }
