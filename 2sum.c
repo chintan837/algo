6b261307de436912817f3875bed15248fda2a80a
Chintan Shah <chintan837@gmail.com>
2017-04-26 14:53:08 -0700

    binary tree working, trying out slow algo

--- 2sum.c
+++ 2sum.c
@@ -29,6 +29,18 @@ struct bst {
 	struct bst *right;
 };
 
+int bst_height(struct bst *root) {
+	int ret = 0;
+	if (root == NULL)
+		return ret;
+
+	int lheight = bst_height(root->left);
+	int rheight = bst_height(root->right);
+
+	(lheight > rheight)? (ret = lheight): (ret = rheight);
+	return ret+1;
+}
+
 struct bst *bst_insert(int data, struct bst *root) {
 	if (root == NULL) {
 		struct bst *newnode = malloc(sizeof(struct bst));
@@ -82,25 +94,48 @@ int main(int argc, char *argv[]) {
 	size_t read = 0;
 	ssize_t len = -1;
 	long int num = 0;
+	long int array[1000000];
 	int count = 0;
 	struct bst *btree = NULL;
 	fp = fopen("algo1-programming_prob-2sum.txt", "r");
 
 	while((len = getline(&line, &read, fp)) != -1) {
 		sscanf(line, "%ld\n", &num);
+		array[count] = num;
 		btree = bst_insert(num, btree);
 		count++;
 	}
 
 	printf("\n# of objects n: %d\n", count);
+	int height = bst_height(btree);
+	printf("Height of the btree: %d\n", height);
 
+#if 0
 	while ((scanf("%ld", &num) == 1)) {
-		printf("Enter number to search: (any other key to quit)");
+		printf("Enter number to search (any other key to quit): ");
 		if (bst_search(num, btree))
 			printf("%ld found\n", num);
 		else
 			printf("%ld not found\n", num);
 	}
+#endif
+
+	count = 0;
+	long int x, y;
+	for (int t = -10000; t <= 10000; t++) {
+		for (int i = 0; i < 1000000; i++) {
+			x = array[i];
+			y = t - x;
+			if (x == y)
+				continue;
+			if (bst_search(y, btree)) {
+				printf("Found sum t: %d with x: %ld and y:%ld\n", t, x, y);
+				count++;
+				break;
+			}
+		}
+	}
+	printf("Number of unique t's: %d\n", count);
 
 	bst_destroy(btree);
 	free(line);
