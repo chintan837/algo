22d4880f3bf113384a190961344600818dc17d34
Chintan Shah <chintan837@gmail.com>
2017-05-29 01:22:50 -0700

    knapsack working for small file

--- knapsack.c
+++ knapsack.c
@@ -56,3 +56,61 @@ ADVICE: If you're not getting the correct answer, try debugging your algorithm u
 small test cases. And then post them to the discussion forum!
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct item {
+	long value;
+	long weight;
+}item_t;
+
+static long 
+max (long n1, long n2) {
+	if (n1 > n2)
+		return n1;
+	else
+		return n2;
+}
+
+int main(void) {
+	FILE *fp = fopen("knapsack1.txt", "r");
+	if (!fp)
+		perror("fopen");
+	int W = 0, N = 0;
+	fscanf(fp, "%d %d\n", &W, &N);
+	printf("knapsack_size: %d number_of_items: %d\n", W, N);
+	item_t *items = calloc(N+1, sizeof(item_t));
+	int i = 0, j = 0;
+
+	i = 1;
+	while ((fscanf(fp, "%ld %ld\n", &(items+i)->value, &(items+i)->weight)) != EOF) {
+		printf("v%d: %ld, w%d: %ld\n", i, (items+i)->value, i, (items+i)->weight);
+		i++;
+	}
+
+	long **A = (long **)calloc(N, sizeof (long *));
+	for (i = 0; i < N+1; i++)
+		A[i] = calloc(W+1, sizeof (long));
+
+	for (j = 0; j <= W; j++)
+		A[0][j] = 0;
+
+	for (i = 1; i <= N; i++)
+		for (j = 0; j <= W; j++) {
+		 	long index = j - ((items+i)->weight);
+			if (index < 0) {
+				A[i][j] = A[i-1][j];
+			} else { 
+				A[i][j] = max(A[i-1][j], A[i-1][index]+(items+i)->value);
+			}
+		}
+
+	printf("value of knapsack: %ld\n", A[N][W]);
+
+	for (i = 0; i < N; i++)
+		free(A[i]);
+	free(A);
+	free(items);
+	fclose(fp);
+	return 0;
+}
